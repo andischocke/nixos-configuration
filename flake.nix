86cc824413656bdfd8b15bf0cb9bdd5f1481930d
Andreas <Andreas.Henneberger@outlook.com>
2026-05-02 22:05:13 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
