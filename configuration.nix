7692456b4f345d96b4433296b0ed7d4c7aa18f7b
Andreas <Andreas.Henneberger@outlook.com>
2026-05-03 15:07:25 +0200

    Enable Visual Studio Code

--- configuration.nix
+++ configuration.nix
@@ -69,18 +69,20 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    nixfmt
     oo7
     oo7-portal
     oo7-server
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.vscode = {
+    enable = true;
+    extensions = [
+      pkgs.vscode-extensions.github.copilot-chat
+      pkgs.vscode-extensions.jnoortheen.nix-ide
+      pkgs.vscode-extensions.redhat.vscode-xml
+    ];
+  };
 
   # List services that you want to enable:
 
