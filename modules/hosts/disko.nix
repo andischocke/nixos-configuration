5ba50bd13195388fb5466453bfa55fcc55e904fa
Andreas <Andreas.Henneberger@outlook.com>
2026-06-27 21:44:59 +0200

    Restructure disko disk configuration hierarchy

--- modules/hosts/disko.nix
+++ modules/hosts/disko.nix
@@ -1,68 +1,70 @@
 {
   disko.devices = {
     # Disk devices
-    disk.main = {
-      device = "/dev/disk/by-id/nvme-KINGSTON_RBUSNS8154P3512GJ_50026B76826F6223";
-      type = "disk";
+    disk = {
+      nvme = {
+        device = "/dev/disk/by-id/nvme-KINGSTON_RBUSNS8154P3512GJ_50026B76826F6223";
+        type = "disk";
 
-      content = {
-        type = "gpt";
-        partitions = {
-          # Extensible Firmware Interface System Partition
-          esp = {
-            name = "ESP";
-            size = "1G";
-            type = "EF00";
+        content = {
+          type = "gpt";
+          partitions = {
+            # Extensible Firmware Interface System Partition
+            esp = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
 
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [
-                "umask=0077"
-              ];
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "umask=0077"
+                ];
+              };
             };
-          };
 
-          # Swap partition
-          swap = {
-            name = "swap";
-            size = "16G";
+            # Swap partition
+            swap = {
+              name = "swap";
+              size = "16G";
 
-            content = {
-              type = "swap";
-              resumeDevice = true;
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
             };
-          };
 
-          # Root partition
-          root = {
-            name = "root";
-            size = "100%";
+            # Root partition
+            root = {
+              name = "root";
+              size = "100%";
 
-            content = {
-              type = "btrfs";
-              extraArgs = [
-                "-f"
-              ];
+              content = {
+                type = "btrfs";
+                extraArgs = [
+                  "-f"
+                ];
 
-              subvolumes = {
-                nix = {
-                  mountpoint = "/nix";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "subvol=nix"
-                  ];
-                };
+                subvolumes = {
+                  nix = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "subvol=nix"
+                    ];
+                  };
 
-                persistent = {
-                  mountpoint = "/persistent";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "subvol=persistent"
-                  ];
+                  persistent = {
+                    mountpoint = "/persistent";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "subvol=persistent"
+                    ];
+                  };
                 };
               };
             };
